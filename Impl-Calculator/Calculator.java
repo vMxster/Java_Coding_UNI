589a5774efd5b8d2ae8c766e9a4cfa1511d040b5
Martin <124467746+vMxster@users.noreply.github.com>
2023-09-30 17:43:22 +0200

    Elaborate Number 3 - OOP Course

---
+++ Impl-Calculator/Calculator.java
@@ -0,0 +1,22 @@
+class Calculator {
+	double add (double num1, double num2) {
+		double num;
+		num = num1 + num2;
+		return num;
+	}
+	double sub (double num1, double num2) {
+		double num;
+		num = num1 - num2;
+		return num;
+	}
+	double mul (double num1, double num2) {
+		double num;
+		num = num1 * num2;
+		return num;
+	}
+	double div (double num1, double num2) {
+		double num;
+		num = num1 / num2;
+		return num;
+	}
+}
