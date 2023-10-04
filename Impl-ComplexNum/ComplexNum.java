106c22a70fdcec845b8cc1505f98e05e55f4e605
Martin <124467746+vMxster@users.noreply.github.com>
2023-10-04 19:38:18 +0200

    Elaborate Number 2 - OOP Course

--- Impl-ComplexNum/ComplexNum.java
+++ Impl-ComplexNum/ComplexNum.java
@@ -20,15 +20,14 @@ public class ComplexNum {
     }
 
     String toStringRep() {
-        if (this.im>=0 && this.re>=0) {
-            return (int) this.re + "+" + (int) this.im + "i";
-        }
-        if (this.im>=0 && this.re<=0) {
-            return (int) this.re + "+" + (int) this.im + "i";
-        }
-        if (this.im<=0 || this.re<=0) {
-            return (int) this.re + "" + (int) this.im + "i";
-        }
-        return "Error";
+        return im == 0
+            // Special case: real number.
+            ? re + ""
+            // Stringify real part if present
+            : (re == 0 ? "" : re)
+                // Add + signum if required (if negative, - is added automatically)
+                + (re != 0 && im > 0 ? "+" : "")
+                // Add imaginary part, handle +-1
+                + (im == 1 ? "" : (im == -1 ? "-" : im + "")) + "i";
     }
 }
