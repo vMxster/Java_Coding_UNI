ecd9c1c7d413f0a2b960eaa128a2457da4a611a1
Martin <124467746+vMxster@users.noreply.github.com>
2023-10-04 19:46:57 +0200

    Elaborate Number 5 - OOP Course

--- Impl-ComplexNum-Calculator/TestComplexNumCalculator.java
+++ Impl-ComplexNum-Calculator/TestComplexNumCalculator.java
@@ -1,40 +1,42 @@
 class TestComplexNumCalculator {
   public static void main(String[] args) {
-      /*
-       * 1) Testare la classe ComplexNumCalculator con le seguenti operazioni
-       *    tra numeri complessi:
-       *
-       * - add(1+2i, 2+3i) = 3+5i
-       *
-       * - sub(4+5i, 6+7i) = -2-2i
-       *
-       * - mul(8+2i, 3-i) = 26 - 2i
-       *
-       * - ... altre a piacere
-       *
-       * 2) Verificare il corretto valore dei campi nOpDone, lastRes
-       *
-       * 3) Fare altre prove con operazioni a piacere
-       */
-	   
-	   ComplexNumCalculator calculator = new ComplexNumCalculator();
-	   calculator.build();
-	   ComplexNum num1 = new ComplexNum();
-	   num1.build(1, 2);
-	   ComplexNum num2 = new ComplexNum();
-	   num2.build(2, 3);
-	   System.out.println("Add 1+2i with 2+3i: " + calculator.add(num1, num2));
-	   System.out.println("Number of Operations Done: " + calculator.nOpDone);
-	   if (calculator.nOpDone > 1) System.out.println("Last Result: " + calculator.lastRes.toStringRep());
-	   num1.build(4, 5);
-	   num2.build(6, 7);
-	   System.out.println("Sub 4+5i with 6+7i: " + calculator.sub(num1, num2));
-	   System.out.println("Number of Operations Done: " + calculator.nOpDone);
-	   if (calculator.nOpDone > 1) System.out.println("Last Result: " + calculator.lastRes.toStringRep());
-	   num1.build(8, 2);
-	   num2.build(3, -1);
-	   System.out.println("Mul 8+2i with 3-i: " + calculator.mul(num1, num2));
-	   System.out.println("Number of Operations Done: " + calculator.nOpDone);
-	   if (calculator.nOpDone > 1) System.out.println("Last Result: " + calculator.lastRes.toStringRep());
+      ComplexNumCalculator calculator = new ComplexNumCalculator();
+      calculator.build();
+
+      ComplexNumber n1 = new ComplexNumber();
+      ComplexNumber n2 = new ComplexNumber();
+      ComplexNumber result;
+
+      // add(1+2i, 2+3i) = 3+5i
+      n1.build(1, 2);
+      n2.build(2, 3);
+      result = calculator.add(n1, n2);
+      System.out.println("Result of add(1+2i, 2+3i) = " + result.toStringRep() + "\n");
+      System.out.println("No. operations done = " + calculator.nOpDone + "\n");
+      System.out.println("Last res = " + calculator.lastRes.toStringRep() + "\n");
+
+      // sub(4+5i, 6+7i) = -2-2i
+      n1.build(4, 5);
+      n2.build(6, 7);
+      result = calculator.sub(n1, n2);
+      System.out.println("Result of sub(4+5i, 6+7i) = " + result.toStringRep() + "\n");
+      System.out.println("No. operations done = " + calculator.nOpDone + "\n");
+      System.out.println("Last res = " + calculator.lastRes.toStringRep() + "\n");
+
+      // mul(8+2i, 3-i) = 26-2i
+      n1.build(8, 2);
+      n2.build(3, -1);
+      result = calculator.mul(n1, n2);
+      System.out.println("Result of mul(8+2i, 3-i) = " + result.toStringRep() + "\n");
+      System.out.println("No. operations done = " + calculator.nOpDone + "\n");
+      System.out.println("Last res = " + calculator.lastRes.toStringRep() + "\n");
+
+      // div(8+i, 2-i) = 3+2i
+      n1.build(8, 1);
+      n2.build(2, -1);
+      result = calculator.div(n1, n2);
+      System.out.println("Result of div(8+i, 2-i) = " + result.toStringRep() + "\n");
+      System.out.println("No. operations done = " + calculator.nOpDone + "\n");
+      System.out.println("Last res = " + calculator.lastRes.toStringRep() + "\n");
   }
 }
