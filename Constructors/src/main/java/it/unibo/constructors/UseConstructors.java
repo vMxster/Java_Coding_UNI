8486ef3ae05c74e6f29f1b9b94cd28912e305020
Martin <124467746+vMxster@users.noreply.github.com>
2023-10-07 21:52:48 +0200

    Elaborate Number 6 - OOP Course

--- Constructors/src/main/java/it/unibo/constructors/UseConstructors.java
+++ Constructors/src/main/java/it/unibo/constructors/UseConstructors.java
@@ -10,46 +10,47 @@ class UseConstructors {
 
         // 1) Creare lo studente Mario Rossi, matricola 1014, anno
         // immatricolazione 2013
+        final Student marioRossi = new Student(1014, "Mario", "Rossi", 2013);
+        marioRossi.printStudentInfo();
 
         // 2) Creare lo studente Luca Bianchi, matricola 1018, anno
         // immatricolazione 2010
+        final Student lucaBianchi = new Student(1018, "Luca", "Bianchi", 2010);
+        lucaBianchi.printStudentInfo();
 
         // 3) Creare lo studente Peppino Vitiello, matricola 1019, anno
         // immatricolazione 2012
+        final Student peppinoVitiello = new Student(1019, "Peppino", "Vitiello", 2012);
+        peppinoVitiello.printStudentInfo();
 
         // 4) Creare lo studente Luca Verdi, matricola 1020, anno
         // immatricolazione 2013
+        final Student lucaVerdi = new Student(1020, "Luca", "Verdi", 2013);
+        lucaVerdi.printStudentInfo();
 
         // 5) Creare un treno con 300 posti di cui 100 in prima classe 200 in
         // seconda classe
+        final Train t1 = new Train(300, 100, 200);
+        t1.printTrainInfo();
 
         // 6) Creare un treno con 1200 posti di cui 50 in prima classe 1050 in
         // seconda classe
+        final Train t2 = new Train(1200, 50, 1050);
+        t2.printTrainInfo();
 
         // 7) Creare un treno con 500 posti, tutti di seconda classe
+        final Train t3 = new Train(500, 0, 500);
+        t3.printTrainInfo();
 
         // 8) Creare un treno con numero di posti di default
+        final Train t4 = new Train();
+        t4.printTrainInfo();
 
         // 9) Verificare che il numero di posti
         // di default sia consistente (ossia che ci sia un numero positivo di
         // posti totali, e che la somma dei posti in prima e seconda classe dia
         // il totale dei posti sul treno).
-
-        Student marioRossi = new Student("Mario", "Rossi", 1014, 2013); 
-        Student lucaBianchi = new Student("Luca", "Bianchi", 1018, 2010); 
-        Student peppinoVitiello = new Student("Peppino", "Vitiello", 1019, 2012); 
-        Student lucaVerdi = new Student("Luca", "Verdi", 1020, 2013);
-        marioRossi.printStudentInfo();
-        lucaBianchi.printStudentInfo();
-        peppinoVitiello.printStudentInfo();
-        lucaVerdi.printStudentInfo();
-        Train train1 = new Train(300, 100, 200, 0, 0);
-        Train train2 = new Train(1200, 50, 1050, 0, 0);
-        Train train3 = new Train(500, 0, 500, 0, 0);
-        Train train4 = new Train(Train.DEFAULT_TOT_SEATS, Train.DEFAULT_N_FC_SEATS, Train.DEFAULT_N_SC_SEATS, 0, 0);
-        train1.printTrainInfo();
-        train2.printTrainInfo();
-        train3.printTrainInfo();
-        train4.printTrainInfo();
+        System.out.println(t4.getTotalSeats() > 0 ? "OK!" : "There is a bug.");
+        System.out.println(t4.getTotalSeats() == t4.getFirstClassSeats() + t4.getSecondClassSeats() ? "OK!" : "There is a bug.");
     }
 }
