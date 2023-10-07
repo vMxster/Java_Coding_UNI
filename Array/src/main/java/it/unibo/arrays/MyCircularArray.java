d35d3ac2abf2536e31682376a4c3b3abecf1bd5f
Martin <124467746+vMxster@users.noreply.github.com>
2023-10-07 21:55:03 +0200

    Elaborate Number 7 - OOP Course

--- Array/src/main/java/it/unibo/arrays/MyCircularArray.java
+++ Array/src/main/java/it/unibo/arrays/MyCircularArray.java
@@ -2,33 +2,30 @@ package it.unibo.arrays;
 
 class MyCircularArray {
 
-    private final int[] array;
-    private int lastPosition = 0;
+    static final int ARRAY_DEF_SIZE = 10;
+    int[] array;
+    int nextPosition;
 
     MyCircularArray() {
-        this(10);
+        this.array = new int[ARRAY_DEF_SIZE];
+        this.nextPosition = 0;
     }
 
     MyCircularArray(int size) {
-        this.array = new int[10];
+        this.array = new int[size];
+        this.nextPosition = 0;
     }
 
     void add(final int elem) {
-        if (this.lastPosition==this.array.length) {
-            this.lastPosition = 0;
-            this.array[this.lastPosition] = elem;
-            this.lastPosition++;
-        } else {
-            this.array[this.lastPosition] = elem;
-            this.lastPosition++;
-        }
+        this.array[nextPosition] = elem;
+        this.nextPosition = (this.nextPosition + 1) % this.array.length;
     }
 
     void reset() {
-        for(int i=0 ; i<this.array.length ; i++) {
+        for (int i = 0; i < this.array.length; i++) {
             this.array[i] = 0;
         }
-        this.lastPosition = 0;
+        this.nextPosition = 0;
     }
 
     void printArray() {
@@ -42,43 +39,36 @@ class MyCircularArray {
     public static void main(final String[] args) {
 
         // 1) Creare un array circolare di dieci elementi
-        MyCircularArray array = new MyCircularArray(10);
+        MyCircularArray circularArray = new MyCircularArray(10);
 
         // 2) Aggiungere gli elementi da 1 a 10 e stampare il contenuto
         // dell'array circolare
-        array.add(1);
-        array.add(2);
-        array.add(3);
-        array.add(4);
-        array.add(5);
-        array.add(6);
-        array.add(7);
-        array.add(8);
-        array.add(9);
-        array.add(10);
-        array.printArray();
-
+        for (int i = 0; i < 10; i++) {
+            circularArray.add(i);
+        }
+        circularArray.printArray();
+        System.out.println();
+        
         // 3) Aggiungere gli elementi da 11 a 15 e stampare il contenuto
         // dell'array circolare
-        array.add(11);
-        array.add(12);
-        array.add(13);
-        array.add(14);
-        array.add(15);
-        array.printArray();
-
+        for (int i = 11; i <= 15; i++) {
+            circularArray.add(i);
+        }
+        circularArray.printArray();
+        System.out.println();
+        
         // 4) Invocare il metodo reset
-        array.reset();
-
+        circularArray.reset();
+        
         // 5) Stampare il contenuto dell'array circolare
-        array.printArray();
-
+        circularArray.printArray();
+        System.out.println();
+        
         // 6) Aggiungere altri elementi a piacere e stampare il contenuto
         // dell'array circolare
-        array.add(23);
-        array.add(76);
-        array.add(90);
-        array.add(25);
-        array.printArray();
+        circularArray.add(1);
+        circularArray.add(2);
+        circularArray.add(3);
+        circularArray.printArray();
     }
 }
