36d7795f743e1e2f08384834c10b8b9295ebe43e
Martin <124467746+vMxster@users.noreply.github.com>
2023-10-07 21:53:29 +0200

    Elaborate Number 6 - OOP Course

--- Constructors/src/main/java/it/unibo/constructors/UseConstructorsEvolved.java
+++ Constructors/src/main/java/it/unibo/constructors/UseConstructorsEvolved.java
@@ -5,18 +5,17 @@ class UseConstructorsEvolved {
     public static void main(final String[] args) {
         // 1) Creare un treno con numero di posti di default, come nel caso
         // precedente (serve a verificare la nuova implementazione di Train()).
-
-        Train train1 = new Train(Train.DEFAULT_TOT_SEATS, Train.DEFAULT_N_FC_SEATS, Train.DEFAULT_N_SC_SEATS, 0, 0);
-        train1.printTrainInfo();
+        final Train t1 = new Train();
+        t1.printTrainInfo();
 
         // 2) Creare i seguenti treni usando il costruttore Train(int nFCSeats, int
         // nSCSeats)
         // - nFCSeats = 20; nSCSeats= 200;
         // - nFCSeats = 35; nSCSeats= 165;
+        final Train t2 = new Train(20, 200);
+        t2.printTrainInfo();
 
-        Train train2 = new Train(20, 200);
-        Train train3 = new Train(35, 165);
-        train2.printTrainInfo();
-        train3.printTrainInfo();
+        final Train t3 = new Train(35, 165);
+        t3.printTrainInfo();
     }
 }
