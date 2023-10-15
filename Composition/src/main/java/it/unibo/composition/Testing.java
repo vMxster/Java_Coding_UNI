2bdf0762eff4753ffd72915ed13c377a84e6856c
Martin <124467746+vMxster@users.noreply.github.com>
2023-10-15 19:59:12 +0200

    Elaborate Number 9 - OOP Course

--- Composition/src/main/java/it/unibo/composition/Testing.java
+++ Composition/src/main/java/it/unibo/composition/Testing.java
@@ -2,29 +2,42 @@ package it.unibo.composition;
 
 public class Testing {
 
+    private Testing() {
+        /*
+         * Impedisce che si possa creare una classe Testing da fuori.
+         */
+    }
+
     public static void main(final String[] args) {
+
         // 1)Creare 3 studenti a piacere
-        Student studentMarcoRossi = new Student(1, "Marco", "Rossi", "Marcorossi123", 2020);
-        Student studentAndreaRossi = new Student(2, "Andrea", "Rossi", "Andrearossi123", 2020);
-        Student studentMatteoRossi = new Student(3, "Matteo", "Rossi", "Matteorossi1!", 2020);
+        final Student aGentile = new Student(1015, "Alex", "Gentile", "1234", 2012);
+        final Student fBianchi = new Student(1016, "Fiore", "Bianchi", "mamma", 2010);
+        final Student aBracci = new Student(1017, "Andrea", "Bracci", "password", 2012);
+
         // 2)Creare 2 docenti a piacere
-        Professor professorLuigiVerdi = new Professor(5, "Luigi", "Verdi", "Luigiverdi45", null);
-        Professor professorMirkoVerdi = new Professor(6, "Mirko", "Verdi", "Mirkoverdi5", null);
+        final Professor santi = new Professor(2015, "Andrea", "Santi", "mypwd", new String[] { "OOP", "SISOP" });
+        final Professor pianini = new Professor(2015, "Danilo", "Pianini", "mypwd2", new String[] { "OOP", "FINFA" });
+
         // 3) Creare due aulee di esame, una con 100 posti una con 80 posti
-        ExamRoom room1 = new ExamRoom(150, "Stanza per Scienze", false, true);
-        ExamRoom room2 = new ExamRoom(250, "Stanza per Informatica", true, true);
-        // 4) Creare due esami, uno con nMaxStudents=10, l'altro con nMaxStudents=2
-        Exam OOP = new Exam(1, 10, "OOP", professorMirkoVerdi, room2, new Student[200], 0);
-        Exam POP = new Exam(2, 2, "POP", professorLuigiVerdi, room1, new Student[150], 0);
+        final ExamRoom vela = new ExamRoom(100, "VELA", true, true);
+        final ExamRoom gpt1 = new ExamRoom(80, "GPT1", true, true);
+
+        // 4) Creare due esami, uno con nMaxStudents=10, l'altro con
+        // nMaxStudents=2
+        final Exam oop = new Exam(10, 10, santi.getCourses()[0], santi, vela);
+        final Exam finfa = new Exam(11, 2, pianini.getCourses()[1], pianini, gpt1);
+
         // 5) Iscrivere tutti e 3 gli studenti agli esami
-        OOP.registerStudent(studentAndreaRossi);
-        OOP.registerStudent(studentMarcoRossi);
-        OOP.registerStudent(studentMatteoRossi);
-        POP.registerStudent(studentAndreaRossi);
-        POP.registerStudent(studentMarcoRossi);
-        POP.registerStudent(studentMatteoRossi);
+        oop.registerStudent(aGentile);
+        oop.registerStudent(fBianchi);
+        oop.registerStudent(aBracci);
+        finfa.registerStudent(aGentile);
+        finfa.registerStudent(fBianchi);
+        finfa.registerStudent(aBracci);
+
         // 6) Stampare in stdout la rapresentazione in stringa dei due esami
-        System.out.println(OOP.toString());
-        System.out.println(POP.toString());
+        System.out.println(oop.toString());
+        System.out.println(finfa.toString());
     }
 }
