8da3fc0ad03768025c4accc0444f123a48b31000
Martin <124467746+vMxster@users.noreply.github.com>
2023-10-15 20:01:35 +0200

    Elaborate Number 10 - OOP Course

--- Shapes/src/main/java/it/unibo/shapes/impl/Circle.java
+++ Shapes/src/main/java/it/unibo/shapes/impl/Circle.java
@@ -1,35 +1,20 @@
 package it.unibo.shapes.impl;
 
 import it.unibo.shapes.api.Shape;
-import java.lang.Math;
 
 public class Circle implements Shape {
 
-    private static final int PERIMETER_CONST = 2;
     private final double radius;
-    private double perimeter;
-    private double area;
 
-    public Circle (final double radius) {
+    public Circle(final double radius) {
         this.radius = radius;
-        this.perimeter = 0;
-        this.area = 0;
     }
 
-    public double getArea () {
-        return this.area;
-    } 
-
-    public double getPerimeter () {
-        return this.perimeter;
-    } 
-
-    public void calculateArea () {
-        this.area = Math.PI * (this.radius * this.radius);
+    public double getArea() {
+        return Math.PI * this.radius * this.radius;
     }
 
-    public void calculatePerimeter () {
-        this.perimeter =  Math.PI * this.radius * PERIMETER_CONST;
+    public double getPerimeter() {
+        return 2 * Math.PI * this.radius;
     }
-
 }
