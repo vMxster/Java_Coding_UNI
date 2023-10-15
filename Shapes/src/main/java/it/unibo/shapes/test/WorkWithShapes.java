8a9eecb6caf2e935ffbf17d2c285a97ebd5acb6f
Martin <124467746+vMxster@users.noreply.github.com>
2023-10-15 20:04:21 +0200

    Elaborate Number 10 - OOP Course

--- Shapes/src/main/java/it/unibo/shapes/test/WorkWithShapes.java
+++ Shapes/src/main/java/it/unibo/shapes/test/WorkWithShapes.java
@@ -1,31 +1,58 @@
 package it.unibo.shapes.test;
 
-import it.unibo.shapes.impl.*;
+import it.unibo.shapes.api.Polygon;
+import it.unibo.shapes.api.Shape;
+import it.unibo.shapes.impl.Circle;
+import it.unibo.shapes.impl.Rectangle;
+import it.unibo.shapes.impl.SimpleTriangle;
+import it.unibo.shapes.impl.Square;
+import it.unibo.shapes.impl.Triangle;
 
 public class WorkWithShapes {
-    public static void main(String[] args) {
-        Circle circle = new Circle(3.0);
-        Rectangle rectangle = new Rectangle(5.0, 8.0);
-        Square square = new Square(10.0);
-        Triangle triangle = new Triangle(10.0, 20.0, 40.0, 15.0);
-        circle.calculateArea();
-        circle.calculatePerimeter();
-        rectangle.calculateArea();
-        rectangle.calculatePerimeter();
-        square.calculateArea();
-        square.calculatePerimeter();
-        triangle.calculateArea();
-        triangle.calculatePerimeter();
-        System.out.println("Circle Area: " + circle.getArea());
-        System.out.println("Rectangle Area: " + rectangle.getArea());
-        System.out.println("Square Area: " + square.getArea());
-        System.out.println("Triangle Area: " + triangle.getArea());
-        System.out.println("Circle Perimeter: " + circle.getPerimeter());
-        System.out.println("Rectangle Perimeter: " + rectangle.getPerimeter());
-        System.out.println("Square Perimeter: " + square.getPerimeter());
-        System.out.println("Triangle Perimeter: " + triangle.getPerimeter());
-        System.out.println("Number of Rectangle Edges: " + rectangle.getEdgeCount());
-        System.out.println("Number of Square Edges: " + square.getEdgeCount());
-        System.out.println("Number of Triangle Edges: " + triangle.getEdgeCount());
+
+    private static final double CIRCLE_RADIUS = 3d;
+    private static final double SQUARE_EDGE = 5d;
+    private static final double RECT_WIDTH = 2d;
+    private static final double RECT_HEIGHT = 4d;
+    private static final double TRIANGLE_EDGE_1 = 4d;
+    private static final double TRIANGLE_EDGE_2 = 3d;
+    private static final double TRIANGLE_EDGE_3 = 5.2;
+    private static final double TRIANGLE_H = 3d; // approximation
+
+    private WorkWithShapes() { }
+
+    private static String shapeDetails(final Shape s) {
+        return "Perimeter = " + s.getPerimeter() + ", area = " + s.getArea();
+    }
+
+    public static void main(final String[] args) {
+        final Shape circle = new Circle(CIRCLE_RADIUS);
+        final Polygon square = new Square(SQUARE_EDGE);
+        final Polygon rectangle = new Rectangle(RECT_WIDTH, RECT_HEIGHT);
+        final Polygon scaleneTriangle = new SimpleTriangle(
+            TRIANGLE_EDGE_1,
+            TRIANGLE_EDGE_2,
+            TRIANGLE_EDGE_3,
+            TRIANGLE_H
+        );
+        final Polygon scaleneTriangle2 = new Triangle(TRIANGLE_EDGE_1, TRIANGLE_EDGE_2, TRIANGLE_EDGE_3);
+        System.out.println("Circle with radius " + CIRCLE_RADIUS + ": " + shapeDetails(circle));
+        System.out.println("Square with edge " + SQUARE_EDGE + ": " + shapeDetails(square));
+        System.out.println(
+            "Rectangle with edges (" + RECT_WIDTH + ", " + RECT_HEIGHT + "): " + shapeDetails(rectangle)
+        );
+        System.out.println(
+            "Scalene triangle with parameters ("
+            + TRIANGLE_EDGE_1 + ", "
+            + TRIANGLE_EDGE_2 + ", "
+            + TRIANGLE_EDGE_3 + ", "
+            + TRIANGLE_H
+            + "): " + shapeDetails(scaleneTriangle)
+        );
+        System.out.println(
+            "Scalene triangle with parameters ("
+            + TRIANGLE_EDGE_1 + ", " + TRIANGLE_EDGE_2 + ", " + TRIANGLE_EDGE_3 + "): "
+            + shapeDetails(scaleneTriangle2)
+        );
     }
 }
