1db76382f24f99544f23e7c6474f34d3b06b3c45
Martin <124467746+vMxster@users.noreply.github.com>
2023-10-15 19:58:33 +0200

    Elaborate Number 9 - OOP Course

--- Composition/src/main/java/it/unibo/composition/Exam.java
+++ Composition/src/main/java/it/unibo/composition/Exam.java
@@ -3,67 +3,74 @@ package it.unibo.composition;
 import java.util.Arrays;
 
 public class Exam {
-    
+
     private final int id;
     private final int maxStudents;
+    private int registeredStudentsCount;
     private final String courseName;
+    private Student[] students; // Expanding array!
     private final Professor professor;
     private final ExamRoom room;
-    private final Student[] students;
-    int registeredStudents;
 
-
-    public Exam (final int id, final int maxStudents, final String courseName, final Professor professor, final ExamRoom room, final Student[] students, final int registeredStudents) {
+    public Exam(final int id, final int nMaxStudents, final String courseName, final Professor professor, final ExamRoom room) {
         this.id = id;
-        this.maxStudents = maxStudents;
+        this.maxStudents = nMaxStudents;
+        this.registeredStudentsCount = 0;
         this.courseName = courseName;
         this.professor = professor;
         this.room = room;
-        this.students = students;
-        this.registeredStudents = registeredStudents;
+        this.students = new Student[0];
+    }
+
+    private void expand() {
+        students = Arrays.copyOf(students, students.length + 1);
+    }
+
+    public void registerStudent(final Student stud) {
+        if (registeredStudentsCount < maxStudents) {
+            expand();
+            students[registeredStudentsCount] = stud;
+            registeredStudentsCount++;
+        }
+    }
+
+    public String toString() {
+        return "Exam ["
+            + "id=" + this.id
+            + ", maxStudents=" + this.maxStudents
+            + ", registeredStudentsCount=" + this.registeredStudentsCount
+            + ", courseName=" + this.courseName
+            + ", students=" + Arrays.toString(this.students)
+            + ", professor=" + this.professor
+            + ", room=" + this.room.toString()
+            + "]";
     }
 
     public int getId() {
         return this.id;
     }
 
-    public int getMaxStudents() {
+    public int getNMaxStudents() {
         return this.maxStudents;
+    }
+
+    public int getnRegisteredStudents() {
+        return this.registeredStudentsCount;
     }
 
     public String getCourseName() {
         return this.courseName;
     }
 
+    public Student[] getStudents() {
+        return Arrays.copyOf(this.students, this.students.length);
+    }
+
     public Professor getProfessor() {
         return this.professor;
     }
 
-    public ExamRoom getExamRoom() {
+    public ExamRoom getRoom() {
         return this.room;
-    }
-
-    public Student[] getStudents() {
-        return this.students;
-    }
-
-    public int getRegisteredStudents() {
-        return this.registeredStudents;
-    }
-
-    public void registerStudent (Student student) {
-        if (students.length==maxStudents) {
-            return;
-        } else if (registeredStudents==0){
-            students[registeredStudents] = student;
-            registeredStudents++;
-        } else {
-            students[registeredStudents-1] = student;
-            registeredStudents++;
-        }
-    }
-    
-    public String toString () {
-        return "[ " + id + " ] [ " + maxStudents + " ] [ " + courseName + " ] " + professor.toString() + " " + room.toString() + " " + Arrays.toString(students) + " [ " + registeredStudents + " ]";
     }
 }
