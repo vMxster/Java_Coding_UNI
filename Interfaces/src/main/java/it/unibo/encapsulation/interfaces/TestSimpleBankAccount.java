4d787e7a38dec818f0845f6c4cb189c4ad2912ee
Martin <124467746+vMxster@users.noreply.github.com>
2023-10-15 19:56:24 +0200

    Elaborate Number 8 - OOP Course

--- Interfaces/src/main/java/it/unibo/encapsulation/interfaces/TestSimpleBankAccount.java
+++ Interfaces/src/main/java/it/unibo/encapsulation/interfaces/TestSimpleBankAccount.java
@@ -9,35 +9,39 @@ public class TestSimpleBankAccount {
     }
 
     public static void main(final String[] args) {
+
         // 1) Creare l' AccountHolder relativo a Andrea Rossi con id 1
-        AccountHolder accountAndreaRossi = new AccountHolder("Andrea","Rossi",1);
+        final AccountHolder aRossi = new AccountHolder("Andrea", "Rossi", 1);
         // 2) Creare l' AccountHolder relativo a Alex Bianchi con id 2
-        AccountHolder accountAlexBianchi = new AccountHolder("Alex","Bianchi",2);
+        final AccountHolder aBianchi = new AccountHolder("Alex", "Bianchi", 2);
+
         // 3) Creare i due SimpleBankAccount corrispondenti
-        SimpleBankAccount accountBankAndreaRossi = new SimpleBankAccount(1, 0);
-        SimpleBankAccount accountBankAlexBianchi = new SimpleBankAccount(2, 0);
+        final SimpleBankAccount rossisAccount = new SimpleBankAccount(aRossi.getUserID(), 0);
+        final SimpleBankAccount bianchisAccount = new SimpleBankAccount(aBianchi.getUserID(), 0);
+
         // 4) Effettuare una serie di depositi e prelievi
-        accountBankAlexBianchi.deposit(2, 500);
-        accountBankAlexBianchi.deposit(2, 200);
-        accountBankAlexBianchi.withdraw(2, 300);
-        accountBankAlexBianchi.deposit(2, 500);
-        accountBankAlexBianchi.withdraw(2, 200);
-        accountBankAndreaRossi.deposit(1, 300);
-        accountBankAndreaRossi.deposit(1, 200);
-        accountBankAndreaRossi.withdraw(1, 400);
-        accountBankAndreaRossi.deposit(1, 500);
-        accountBankAndreaRossi.withdraw(1, 200);
+        rossisAccount.deposit(aRossi.getUserID(), 10000);
+        rossisAccount.depositFromATM(aRossi.getUserID(), 10000);
+        bianchisAccount.deposit(aBianchi.getUserID(), 10000);
+        bianchisAccount.depositFromATM(aBianchi.getUserID(), 10000);
+        rossisAccount.withdrawFromATM(aRossi.getUserID(), 500);
+        rossisAccount.withdraw(aRossi.getUserID(), 200);
+        bianchisAccount.withdrawFromATM(aBianchi.getUserID(), 500);
+        bianchisAccount.withdrawFromATM(aBianchi.getUserID(), 500);
+
         /*
          * 5) Stampare a video l'ammontare dei due conti e verificare la
          * correttezza del risultato
          */
-        System.out.println(accountBankAlexBianchi.getBalance());
-        System.out.println(accountBankAndreaRossi.getBalance());
+        System.out.println("Account 1 balance is: " + rossisAccount.getBalance());
+        System.out.println("Account 2 balance is: " + bianchisAccount.getBalance());
+
         // 6) Provare a prelevare fornendo un id utente sbagliato
-        accountBankAlexBianchi.deposit(1, 500);
-        accountBankAndreaRossi.deposit(2, 500);
+        rossisAccount.withdraw(7, 340);
+        bianchisAccount.deposit(8, 900);
+
         // 7) Controllare nuovamente l'ammontare
-        System.out.println(accountBankAlexBianchi.getBalance());
-        System.out.println(accountBankAndreaRossi.getBalance());
+        System.out.println("\nAccount 1 balance is: " + rossisAccount.getBalance());
+        System.out.println("Account 2 balance is: " + bianchisAccount.getBalance());
     }
 }
