b2953ffe1aa81e7527032d5241d764ccd6a4756b
Martin <124467746+vMxster@users.noreply.github.com>
2023-10-15 19:58:02 +0200

    Elaborate Number 9 - OOP Course

--- Composition/src/main/java/it/unibo/composition/Professor.java
+++ Composition/src/main/java/it/unibo/composition/Professor.java
@@ -1,5 +1,7 @@
 package it.unibo.composition;
 
+import java.util.Arrays;
+
 public class Professor implements User {
 
     private static final String DOT = ".";
@@ -9,20 +11,53 @@ public class Professor implements User {
     private final String password;
     private String[] courses;
 
-    public Professor (final int id, final String name, final String surname, final String password, final String[] courses) {
+    public Professor(
+        final Integer id,
+        final String name,
+        final String surname,
+        final String password,
+        final String[] courses
+    ) {
         this.id = id;
         this.name = name;
         this.surname = surname;
         this.password = password;
-        this.courses = courses;
+        this.courses = Arrays.copyOf(courses, courses.length);
     }
 
-    public void replaceCourse(final String nameCourse, final int i) {
-        this.courses[i] = nameCourse;
+    public Integer getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getSurname() {
+        return this.surname;
+    }
+
+    public String[] getCourses() {
+        return Arrays.copyOf(this.courses, this.courses.length);
+    }
+
+    public void replaceCourse(final String subject, final int index) {
+        if (index < this.courses.length) {
+            this.courses[index] = subject;
+        }
     }
 
     public void replaceAllCourses(final String[] newCourses) {
-        this.courses = newCourses;
+        this.courses = Arrays.copyOf(newCourses, newCourses.length);
+    }
+
+    public String toString() {
+        return "Professor ["
+            + "id=" + this.id
+            + ", name=" + this.name
+            + ", surname=" + this.surname
+            + ", subjects=" + Arrays.toString(this.courses)
+            + "]";
     }
 
     public String getUsername() {
@@ -35,13 +70,5 @@ public class Professor implements User {
 
     public String getDescription() {
         return this.toString();
-    }
-
-    public String toString() {
-        return "[Professor ["
-            +"id=" + this.id
-            + ", name=" + this.name
-            + ", surname=" + this.surname
-            + "]]";
     }
 }
