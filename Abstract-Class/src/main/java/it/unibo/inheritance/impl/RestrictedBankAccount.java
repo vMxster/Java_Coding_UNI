153a18955e54402f528b57e1b94b3e493de832e5
Martin <124467746+vMxster@users.noreply.github.com>
2023-10-20 20:52:47 +0200

    Elaborate Number 11 - OOP Course

--- Abstract-Class/src/main/java/it/unibo/inheritance/impl/RestrictedBankAccount.java
+++ Abstract-Class/src/main/java/it/unibo/inheritance/impl/RestrictedBankAccount.java
@@ -1,37 +1,20 @@
 package it.unibo.inheritance.impl;
 
-import it.unibo.inheritance.api.*;
+import it.unibo.inheritance.api.AccountHolder;
 
 public class RestrictedBankAccount extends AbstractBankAccount {
-    private static final double TRANSACTION_FEE = 0.1;
+
+    public static final double TRANSACTION_FEE = 0.1;
 
     public RestrictedBankAccount(final AccountHolder accountHolder, final double balance) {
         super(accountHolder, balance);
     }
 
-    public void chargeManagementFees(final int usrID) {
-        final double feeAmount = MANAGEMENT_FEE + getTransactionsCount() * TRANSACTION_FEE;
-        if (checkUser(usrID) && isWithDrawAllowed(feeAmount)) {
-            this.setBalance(getBalance() - feeAmount);
-            this.resetTransactions();
-        }
+    protected double computeFees() {
+        return MANAGEMENT_FEE + (getTransactionsCount() * TRANSACTION_FEE);
     }
 
-    public void withdraw(final int usrID, final double amount) {
-        if (isWithDrawAllowed(amount)) {
-            transactionOp(this.getAccountHolder().getUserID(), -amount);
-        }
-    }
-
-    protected boolean checkUser(final int id) {
-        return this.getAccountHolder().getUserID() == id;
-    }
-
-    protected double computeFee() {
-        return MANAGEMENT_FEE;
-    }
-
-    protected boolean isWithDrawAllowed(double amount) {
-        return getBalance() > amount;
+    protected boolean isWithdrawAllowed(final double amount) {
+        return getBalance() >= amount;
     }
 }
