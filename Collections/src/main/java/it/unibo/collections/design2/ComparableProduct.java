08e8b08640a149ec9992a2766915b14c2f7acbcd
Martin <124467746+vMxster@users.noreply.github.com>
2023-10-30 18:08:47 +0100

    Elaborate Number 13 - OOP Course

--- Collections/src/main/java/it/unibo/collections/design2/ComparableProduct.java
+++ Collections/src/main/java/it/unibo/collections/design2/ComparableProduct.java
@@ -4,27 +4,27 @@ import java.lang.String;
 import it.unibo.collections.design.ProductImpl;
 import it.unibo.collections.design.api.Product;
 
-public class ComparableProduct implements Comparable<Product>, Product {
+/**
+ *
+ */
+public class ComparableProduct extends ProductImpl implements Comparable<Product> {
 
-    private final Product product;
-
-    public ComparableProduct(String name, double quantity) {
-        this.product = new ProductImpl(name, quantity);
+    /**
+     * @param name
+     *            the product name
+     * @param quantity
+     *            the product quantity
+     */
+    public ComparableProduct(final String name, final double quantity) {
+        super(name, quantity);
     }
 
-    public int compareTo (Product o) {
-        return this.product.getName().compareTo(o.getName());
-    }
-
-    public String getName() {
-        return product.getName();
-    }
-
-    public double getQuantity() {
-        return product.getQuantity();
-    }
-
-    public String toString() {
-        return product.getName();
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int compareTo(final Product o) {
+        return getName().compareTo(o.getName());
     }
 }
+
