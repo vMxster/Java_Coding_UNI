8487b65cc5e3216a14ad0c562a0b76481478081b
Martin <124467746+vMxster@users.noreply.github.com>
2023-10-30 18:09:36 +0100

    Elaborate Number 13 - OOP Course

--- Collections/src/main/java/it/unibo/collections/design2/OrderedWarehouse.java
+++ Collections/src/main/java/it/unibo/collections/design2/OrderedWarehouse.java
@@ -1,13 +1,19 @@
 package it.unibo.collections.design2;
 
 import java.util.Set;
+import java.util.TreeSet;
 
 import it.unibo.collections.design.WarehouseImpl;
 import it.unibo.collections.design.api.Product;
 
 public class OrderedWarehouse extends WarehouseImpl {
 
+    /**
+     * {@inheritDoc}
+     */
+    @Override
     public Set<Product> allProducts() {
-        return super.allProducts();
+        return new TreeSet<>(super.allProducts());
     }
+
 }
